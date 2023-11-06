2dc3227322f9f19023f2bc2d8a65e4976d15b32b
Danny Rivera <samboard@hotmail.com>
2023-11-05 19:21:08 -0500

    ArticlesController solution

--- components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -7,6 +7,7 @@ import org.eclipse.jetty.server.Request;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ArticlesController extends BasicHandler {
     private final ArticleDataGateway gateway;
@@ -21,6 +22,15 @@ public class ArticlesController extends BasicHandler {
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
+                List<ArticleRecord> articleRecord_list = gateway.findAll();
+                List<ArticleInfo> articles = new ArrayList<ArticleInfo>();
+                // Iterating over the articleRecord_list
+                for (int i = 0; i < articleRecord_list.size(); i++) {
+                    ArticleRecord curr_articleRecord = articleRecord_list.get(i);
+                    ArticleInfo curr_articleInfo = new ArticleInfo(curr_articleRecord.getId(), curr_articleRecord.getTitle());
+                    articles.add(curr_articleInfo);
+                }
+                writeJsonBody(servletResponse, articles);
 
             }
         });
@@ -28,7 +38,15 @@ public class ArticlesController extends BasicHandler {
         get("/available", List.of("application/json"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
-
+                List<ArticleRecord> articleRecord_list = gateway.findAvailable();
+                List<ArticleInfo> articles = new ArrayList<ArticleInfo>();
+                // Iterating over the articleRecord_list
+                for (int i = 0; i < articleRecord_list.size(); i++) {
+                    ArticleRecord curr_articleRecord = articleRecord_list.get(i);
+                    ArticleInfo curr_articleInfo = new ArticleInfo(curr_articleRecord.getId(), curr_articleRecord.getTitle());
+                    articles.add(curr_articleInfo);
+                }
+                writeJsonBody(servletResponse, articles);
             }
         });
     }
