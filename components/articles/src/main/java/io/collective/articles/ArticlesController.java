8255f1b3d447151e7a283507707a68ea7736e3f4
Danny Rivera <samboard@hotmail.com>
2023-11-05 20:13:58 -0500

    Endpoints solution

--- components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -22,32 +22,27 @@ public class ArticlesController extends BasicHandler {
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
-                List<ArticleRecord> articleRecord_list = gateway.findAll();
                 List<ArticleInfo> articles = new ArrayList<ArticleInfo>();
-                // Iterating over the articleRecord_list
-                for (int i = 0; i < articleRecord_list.size(); i++) {
-                    ArticleRecord curr_articleRecord = articleRecord_list.get(i);
-                    ArticleInfo curr_articleInfo = new ArticleInfo(curr_articleRecord.getId(), curr_articleRecord.getTitle());
-                    articles.add(curr_articleInfo);
-                }
+                populate_articles(gateway.findAll(), articles);
                 writeJsonBody(servletResponse, articles);
-
             }
         });
 
         get("/available", List.of("application/json"), request, servletResponse, () -> {
 
             { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
-                List<ArticleRecord> articleRecord_list = gateway.findAvailable();
                 List<ArticleInfo> articles = new ArrayList<ArticleInfo>();
-                // Iterating over the articleRecord_list
-                for (int i = 0; i < articleRecord_list.size(); i++) {
-                    ArticleRecord curr_articleRecord = articleRecord_list.get(i);
-                    ArticleInfo curr_articleInfo = new ArticleInfo(curr_articleRecord.getId(), curr_articleRecord.getTitle());
-                    articles.add(curr_articleInfo);
-                }
+                populate_articles(gateway.findAvailable(), articles);
                 writeJsonBody(servletResponse, articles);
             }
         });
     }
+
+    private void populate_articles(List<ArticleRecord> articleRecord_list, List<ArticleInfo> articles) {
+        for (int i = 0; i < articleRecord_list.size(); i++) {
+            ArticleRecord curr_articleRecord = articleRecord_list.get(i);
+            ArticleInfo curr_articleInfo = new ArticleInfo(curr_articleRecord.getId(), curr_articleRecord.getTitle());
+            articles.add(curr_articleInfo);
+        }
+    } 
 }
